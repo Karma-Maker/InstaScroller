582ee7813a9771baaa6d3d6ef82083acf9e3e1b0
Karma Maker <karmamaker.dev@gmail.com>
2017-07-29 00:51:51 +0300

    Remove useless stuff from main script

--- magic/insta_scroller.py
+++ magic/insta_scroller.py
@@ -6,11 +6,6 @@ import re
 import sys
 import os
 
-try:
-    sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
-except:
-    pass
-
 from viewclient import ViewClient
 
 os.system("magic/bin/adb start-server")
@@ -34,14 +29,9 @@ snapshotIdx = 0
 
 while True:
     content_list.uiScrollable.flingForward()
-
-    # vc = ViewClient(device=device, serialno=serialno)
     vc.dump(content_list.getId())
 
     subtitle = vc.findViewWithText("Sponsored")
-
-    if subtitle is None:
-        subtitle = vc.findViewWithText("Реклама")
 
     if subtitle is not None:
         os.system("magic/bin/adb exec-out screencap -p > snapshots/my_snapshot_{}.png".format(snapshotIdx))
